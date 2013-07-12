f1a4350b8162eabc4d964b151ecb2090df512e20
Zearen Wover <zearen.wover@gmail.com>
2013-07-12 14:35:15 -0400

    Update of work on algo (Sorry, it's hard !)

--- DroidFunge/src/main/java/us/exya/droidfunge/fuckthis/Factory.java
+++
@@ -1,8 +0,0 @@
-package us.exya.droidfunge.fuckthis;
-
-/**
- * Created by zearen on 10/07/13.
- */
-public interface Factory<T> {
-    T getNew();
-}
--- DroidFunge/src/main/java/us/exya/droidfunge/grid/Grid.java
+++ DroidFunge/src/main/java/us/exya/droidfunge/grid/Grid.java
@@ -4,19 +4,11 @@ import android.graphics.Point;
 import android.graphics.Rect;
 
 import java.lang.reflect.Array;
-import java.net.NoRouteToHostException;
-import java.util.Arrays;
-
-import us.exya.droidfunge.fuckthis.Factory;
-import us.exya.droidfunge.fuckthis.NullFactory;
-
-import static us.exya.droidfunge.grid.Direction.*;
 
 /**
  * Created by zearen on 09/07/13.
  */
 public class Grid<T> {
-    protected Factory<T> factory = new NullFactory<T>();
     protected Point sectorSize;
     Class<? extends T> tClass;
 
@@ -26,48 +18,64 @@ public class Grid<T> {
     protected Level curSector;
     protected Rect  curSize;
 
+
     private class Level {
         public int level;
-        public T[][] items = null;
+        private T[][] items = null;
         public Level[][] children = null;
         public Level parent = null;
-        public Point relLoc;
+        public Point relPos;
+        private boolean modified = false;
 
         public Level() {
-            this(new Point(sectorSize.x / 2, sectorSize.y / 2), null);
+            relPos = new Point(sectorSize.x / 2, sectorSize.y / 2);
+            items = (T[][]) Array.newInstance(tClass,
+                    sectorSize.x, sectorSize.y);
+            level = 0;
         }
 
         // Upwards constructor
         public Level(Level child) {
-            relLoc = new Point(sectorSize.x / 2, sectorSize.y / 2);
+            relPos = new Point(sectorSize.x / 2, sectorSize.y / 2);
             children = (Level[][]) Array.newInstance(this.getClass(),
                     sectorSize.x, sectorSize.y);
-            children[child.relLoc.x][child.relLoc.y] = child;
-            child.parent = this;
+            // Only keep track of a child if it has data in it
+            if (child.modified) {
+                children[child.relPos.x][child.relPos.y] = child;
+                child.parent = this;
+            }
             this.level = child.level + 1;
         }
 
         // Downwards constructor
         public Level(Point relLoc, Level level) {
-            this.relLoc = relLoc;
+            this.relPos = relLoc;
             parent = level;
-            // If this is level 1 or this is the initial level, it's a sector
-            // So fill it with actual things
-            if (parent == null || parent.level == 2) {
+            this.level = parent.level - 1;
+            // If this is level 0, it's a sector so fill it with actual things
+            if (parent.level == 1) {
                 items = (T[][]) Array.newInstance(tClass,
                         sectorSize.x, sectorSize.y);
-                this.level = 1;
             }
             else {
                 children = (Level[][]) Array.newInstance(this.getClass(),
                         sectorSize.x, sectorSize.y);
-                this.level = parent.level - 1;
             }
         }
-    }
 
-    public void setFactory(Factory<T> factory) {
-        this.factory = factory;
+        public T getAt(Point pnt) {
+            return items
+                    [rem(pnt.x, sectorSize.x)]
+                    [rem(pnt.y, sectorSize.y)];
+        }
+
+        public void setAt(Point pnt, T item) {
+            items
+                    [rem(pnt.x, sectorSize.x)]
+                    [rem(pnt.y, sectorSize.y)]
+                    = item;
+            modified = item != null;
+        }
     }
 
     public Rect getSize() {
@@ -91,32 +99,77 @@ public class Grid<T> {
         curSize = new Rect(0,0,0,0);
     }
 
+    // Assures modulo is positive
+    private static int rem(int n, int m) {
+        int ret = n % m;
+        if (ret < 0) ret += m;
+        return ret;
+    }
+
+    private Point getSector(Point pnt) {
+        return getSector(pnt, sectorSize);
+    }
+
     private Point getSector(Point pnt, Point size) {
         return new Point(
-                pnt.x / size.x,
-                pnt.y / size.y);
+                Math.abs(pnt.x / size.x),
+                Math.abs(pnt.y / size.y));
     }
 
     // This moves the [curPos] to [dest] creating new
     // Sectors and levels as necessary.
     private void moveTo(Point dest) {
-        Point relDest = new Point(
-                dest.x - curPos.x,
-                dest.y - curPos.y);
-        curSector = zoomOutIn(curSector, relDest);
-        curPos = dest;
-    }
+        if (!getSector(curPos).equals(getSector(dest))) {
+            Point relDest = new Point(
+                    dest.x - curPos.x,
+                    dest.y - curPos.y);
+            Level curLevel = curSector;
+            Point relPos = new Point(
+                    rem((curPos.x - sectorSize.x / 2), sectorSize.x),
+                    rem((curPos.y - sectorSize.y / 2), sectorSize.y));
+            Point size = new Point(1,1);
+            // The direction we're moving
+            boolean xFound = relDest.x == 0;
+            boolean yFound = relDest.y == 0;
+            while (!(xFound && yFound)) {
+                size.x *= sectorSize.x;
+                size.y *= sectorSize.y;
+                if (xFound) {
+                    relDest.x += size.x * relPos.x;
+                }
+                else {
+                    if (relDest.x > 0) {
 
-    private Level zoomOutIn(Level curLevel, Point relDest) {
-        //if (relDest.x ) ;
-        return null;
+                        if (relDest.x <= 0) {
+                            relDest.x = size.x * sectorSize.x + relDest.x - 1;
+                            xFound = true;
+                        }
+                    }
+                }
+                if (curLevel.parent == null) {
+                    curLevel.parent = new Level(curLevel);
+                }
+                curLevel = curLevel.parent;
+            }
+            while (curLevel.level > 0) {
+                int x = relDest.x / size.x;
+                int y = relDest.y / size.y;
+                relDest.x %= size.x;
+                relDest.y %= size.y;
+                if (curLevel.children[x][y] == null) {
+                    curLevel.children[x][y] = new Level(new Point(x,y), curLevel);
+                }
+                curLevel = curLevel.children[x][y];
+                size.x /= sectorSize.x;
+                size.y /= sectorSize.y;
+            }
+        }
+        curPos = dest;
     }
 
     public T getAt(Point at) {
         moveTo(at);
-        return curSector.items
-                [curPos.x % sectorSize.x]
-                [curPos.y % sectorSize.y];
+        return curSector.getAt(curPos);
     }
 
     public void setAt(Point at, T item) {
