6c0e9c73a7a89767fc6bc671f65ceb7eb5c2940a
Zearen Wover <zearen.wover@gmail.com>
2013-07-12 22:44:06 -0400

    Added constructor so it would compile

--- DroidFunge/src/main/java/us/exya/droidfunge/ui/BefungeGridView.java
+++ DroidFunge/src/main/java/us/exya/droidfunge/ui/BefungeGridView.java
@@ -1,9 +1,13 @@
 package us.exya.droidfunge.ui;
 
+import android.content.Context;
 import android.widget.GridView;
 
 /**
  * Created by chris on 7/9/13.
  */
 public class BefungeGridView extends GridView {
+    public BefungeGridView(Context context) {
+        super(context);
+    }
 }
