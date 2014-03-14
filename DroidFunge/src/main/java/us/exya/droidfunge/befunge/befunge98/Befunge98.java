dd29b30e9fe3632cfc3a29a4b7fcaed62813b81f
Zearen Wover <zearen.wover@gmail.com>
2014-03-13 23:29:46 -0400

    Added placeholder implementations.

---
+++ DroidFunge/src/main/java/us/exya/droidfunge/befunge/befunge98/Befunge98.java
@@ -0,0 +1,7 @@
+package us.exya.droidfunge.befunge;
+
+import us.exya.droidfunge.befunge.Befunge;
+
+public class Befunge98 extends Befunge {
+
+}
---
+++ DroidFunge/src/main/java/us/exya/droidfunge/befunge/droidfunge/DroidFunge.java
@@ -0,0 +1,7 @@
+package us.exya.droidfunge.befunge;
+
+import us.exya.droidfunge.befunge.Befunge;
+
+public class DroidFunge extends Befunge {
+
+}
